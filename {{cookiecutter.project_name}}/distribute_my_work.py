6150ec6308ad14ef9afa5962f5af103c4589e3b3
riebeeck.vanniekerk@gmail.com <riebeeck.vanniekerk@gmail.com>
2022-01-26 17:38:58 +0000

    write_path_update
    updating the file write locations on dbfs://

--- {{cookiecutter.project_name}}/distribute_my_work.py
+++ {{cookiecutter.project_name}}/distribute_my_work.py
@@ -19,4 +19,4 @@ def asof_join(l, r):
     return pd.merge_asof(l, r, on="time", by="id")
 df2 = df1.groupby("id").cogroup(df2.groupby("id")).applyInPandas(asof_join, "time int, id int, v1 double, v2 string")
 
-df2.coalesce(1).write.parquet("dbfs:/home/tristen.wentling@databricks.com/pandastest.parquet")
+df2.coalesce(1).write.parquet("dbfs:/home/riebeeck.vanniekerk@databricks.com/databricks_connect_test/pyspark_pandastest.parquet")
